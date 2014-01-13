a5f229740b85705f52ba52316d889db6571edaf5
Elco Jacobs <elcojacobs@gmail.com>
2014-01-13 20:27:25 +0100

    Added eve-docs to repo

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 __author__ = 'Elco'
 
 from eve import Eve
+from flask.ext.bootstrap import Bootstrap
+from eve_docs import eve_docs
 
 app = Eve()
+Bootstrap(app)
+app.register_blueprint(eve_docs, url_prefix='/docs')
 app.run()
